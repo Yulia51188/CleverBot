fc25acf740c29bd69c0e631cd8dd6702fa0519a8
Yulia51188 <sun51188@gmail.com>
2020-09-18 11:05:34 +0300

    Remove detect_intent_texts to seprate module

--- run_telegram_bot.py
+++ run_telegram_bot.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from get_response_from_dialogflow import detect_intent_texts
 from functools import partial
-import dialogflow_v2 as dialogflow
 
 
 def error_callback(update, context, logger):
@@ -34,37 +34,37 @@ def run_bot(token, logger, project_id):
     updater.idle()
 
 
-def detect_intent_texts(project_id, session_id, texts, language_code, logger):
-    """Returns the result of detect intent with texts as inputs.
+# def detect_intent_texts(project_id, session_id, texts, language_code, logger):
+#     """Returns the result of detect intent with texts as inputs.
 
-    Using the same `session_id` between requests allows continuation
-    of the conversation."""
+#     Using the same `session_id` between requests allows continuation
+#     of the conversation."""
 
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    for text in texts:
-        text_input = dialogflow.types.TextInput(
-            text=text, language_code=language_code)
-        query_input = dialogflow.types.QueryInput(text=text_input)
-        response = session_client.detect_intent(
-            session=session, query_input=query_input)
-        logger.debug('=' * 20)
-        logger.debug('Query text: {}'.format(response.query_result.query_text))
-        logger.debug('Detected intent: {} (confidence: {})\n'.format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence))
-        logger.debug('Fulfillment text: {}\n'.format(
-            response.query_result.fulfillment_text)) 
-        fulfillment_text = response.query_result.fulfillment_text
-        return fulfillment_text
+#     session_client = dialogflow.SessionsClient()
+#     session = session_client.session_path(project_id, session_id)
+#     for text in texts:
+#         text_input = dialogflow.types.TextInput(
+#             text=text, language_code=language_code)
+#         query_input = dialogflow.types.QueryInput(text=text_input)
+#         response = session_client.detect_intent(
+#             session=session, query_input=query_input)
+#         logger.debug('=' * 20)
+#         logger.debug('Query text: {}'.format(response.query_result.query_text))
+#         logger.debug('Detected intent: {} (confidence: {})\n'.format(
+#             response.query_result.intent.display_name,
+#             response.query_result.intent_detection_confidence))
+#         logger.debug('Fulfillment text: {}\n'.format(
+#             response.query_result.fulfillment_text)) 
+#         fulfillment_text = response.query_result.fulfillment_text
+#         return fulfillment_text
 
 
 def main():
     load_dotenv()
     logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s - '
-        '%(message)s', level=logging.DEBUG)
+        '%(message)s', level=logging.INFO)
     logger = logging.getLogger('chatbot3_logger')
-    bot_token = os.getenv("BOT_TOKEN")
+    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     project_id = os.getenv("PROGECT_ID")
     run_bot(bot_token, logger, project_id)
 
