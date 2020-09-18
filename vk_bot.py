05becc7e1d84025f9f0ad411763d4dd97516accc
Yulia51188 <sun51188@gmail.com>
2020-09-18 11:33:01 +0300

    Done step 13: create longpolling echobot

--- vk_bot.py
+++ vk_bot.py
@@ -4,8 +4,10 @@ import logging
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+import random
 
-def run_longpolling(vk_group_token, logger):
+
+def run_longpolling_listener(vk_group_token, logger):
     vk_session = vk_api.VkApi(token=vk_group_token)
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
@@ -18,6 +20,24 @@ def run_longpolling(vk_group_token, logger):
             logger.debug(f'Текст: {event.text}')
 
 
+def run_longpolling_echobot(vk_group_token, logger):
+    vk_session = vk_api.VkApi(token=vk_group_token)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            logger.debug(f"Новое сообщение от {event.user_id}: {event.text}")
+            echo(event, vk)
+
+
+def echo(event, vk):
+    vk.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
+
+
 def main():
     load_dotenv()
     logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s - '
@@ -25,7 +45,8 @@ def main():
     logger = logging.getLogger('chatbot3_logger')
     vk_group_token = os.getenv("VK_GROUP_TOKEN")
     project_id = os.getenv("PROGECT_ID")
-    run_longpolling(vk_group_token, logger)
+    # run_longpolling_listener(vk_group_token, logger)
+    run_longpolling_echobot(vk_group_token, logger)
 
 
 if __name__=='__main__':
